fb6cf49944b32fb3f1f9c6445b9c3af045aa142a
Chanathit Thadkaew <chthadka@z2t18c3.42bangkok.com>
2026-09-22 12:55:18 +0700

    Finish cell04 ex03 float.py

---
+++ cell04/ex03/float.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+try:
+    num = float(input("Give me a number: "))
+    if num.is_integer():
+        print("This number is an integer.")
+    else:
+        print("This number is a decimal.")
+except ValueError:
+    pass
