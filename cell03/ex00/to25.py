f2027798daecf06a7e5dc03e24802ceac8b950bf
Chanathit Thadkaew <chthadka@z2t18c3.42bangkok.com>
2026-09-22 10:57:02 +0700

    Finish cell03 ex00 to25.py

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+try:
+    print("Enter a number less than 25")
+    num = int(input())
+    
+    if num > 25:
+        print("Error")
+    else:
+        while num <= 25:
+            print(f"Inside the loop, my variable is {num}")
+            num += 1
+except ValueError:
+    pass
