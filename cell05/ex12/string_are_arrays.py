3ef937770069bf0db946ac74cc22eb68c923d0b9
Chanathit Thadkaew <chthadka@z2t18c3.42bangkok.com>
2026-09-22 14:48:07 +0700

    Finish cell05 ex12 string_are_arrays.py

---
+++ cell05/ex12/string_are_arrays.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+
+if len(sys.argv) == 2:
+    text = sys.argv[1]
+    result = ""
+    for char in text:
+        if char == "z":
+            result += "z"
+    if len(result) > 0:
+        print(result)
+    else:
+        print("none")
+else:
+    print("none")
